074f0766b8c025c345d90063f83836bf03eefd3c
Assma-IBIKAS <Assma-IBIKAS>
2025-10-31 17:07:57 +0100

    update test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,8 @@
-
-import pytest
 from fastapi.testclient import TestClient
-# from main import app
-from fastapi import FastAPI
+from src.main import app
 
-app = FastAPI
-client = app
-
-def test_prediction_risk_status_code(): 
-   data={"prénom": "khadija",
-       "age": 55, 
-       "income": 40000,
-       "gender": "male"}
-   response= client.post("/predict_risk", json=data)
-   assert response.status_code==200
-   json_data =response.json()
-   assert json_data["prediction"] in ["low", "high"]
-   
-   
-def test_predict_risk_invalid_data():
-        invalid_data = {"prenom": 234, 
-                    #   "age" manquant, 
-                    "income": 40000,
-                    "gender": "male"
-                    }
-        response=client.post("/predict_risk", json=invalid_data)
-        assert response.status_code==422
+client = TestClient(app)
+def test_status_code():
+    response = client.get('/patients/predict_risk')
+    # print(response.status_code)
+    assert response.status_code == 200
